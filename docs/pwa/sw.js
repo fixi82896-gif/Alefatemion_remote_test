dfd8ab697f1e633f3b1025d70a9a2a28dd04a9a8
Fixit <fixi82896@gmail.com>
2026-09-20 12:43:41 +0330

    Refresh PWA cache after carousel fix

--- docs/pwa/sw.js
+++ docs/pwa/sw.js
@@ -1,5 +1,5 @@
-const CACHE='alfatemiun-pwa-test-v2';
-const CORE=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icon.svg'];
+const CACHE='alfatemiun-pwa-test-v2-1';
+const CORE=['./','./index.html','./styles.css','./app.js?v=0.2.1','./manifest.webmanifest','./assets/icon.svg'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
